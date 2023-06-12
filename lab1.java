30c466b316a7e304e8f1b321a1b196b6838ca5be
mgiap <10422024@student.vgu.edu.vn>
2023-06-12 16:43:30 +0700

    sum

---
+++ lab1.java
@@ -0,0 +1,8 @@
+class Sum{
+    public static void main(String[] args) {
+        int a = 10, b = 4, sum;
+        sum = a + b;
+        System.out.println("The sum of a and b is " + sum);
+    
+    }
+}
