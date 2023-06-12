6bc3ba39074b590cdbbd7db1687c9f6dbf0eb464
mgiap <10422024@student.vgu.edu.vn>
2023-06-12 16:54:56 +0700

    add subtraction

--- lab1.java
+++ lab1.java
@@ -2,7 +2,9 @@ class Sum{
     public static void main(String[] args) {
         int a = 10, b = 4, sum;
         sum = a + b;
+        subtract = a - b;
+        
         System.out.println("The sum of a and b is " + sum);
-    
+        System.out.println("The sum of a and b is " + subtract);
     }
 }
